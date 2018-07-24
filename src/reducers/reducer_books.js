68af48f7de2c433661b78dc5f2fb24701f65c40a
Mara Shen <maraoshen@gmail.com>
2018-07-25 03:19:54 +1000

    React Redux tutorial. book list

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,7 @@
+export default function() {
+  return [
+    { title: 'You dont know JavaScript', pages: 101},
+    { title: 'Harry Potter', pages: 300},
+    { title: 'Game of Thrones', pages: 10}
+  ]
+}
